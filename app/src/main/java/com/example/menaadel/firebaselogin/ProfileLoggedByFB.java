73118915b238f7c3ff4844a3420e7fc5b41ae105
Mena Adel <menaadel322@gmail.com>
2018-02-17 16:22:12 +0200

    FBLogin

---
+++ app/src/main/java/com/example/menaadel/firebaselogin/ProfileLoggedByFB.java
@@ -0,0 +1,13 @@
+package com.example.menaadel.firebaselogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileLoggedByFB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_logged_by_fb);
+    }
+}
