2a847b31b5c499868fb43cbec7cd3800860f800f
Mena Adel <menaadel322@gmail.com>
2018-02-18 16:23:56 +0200

    Retrieve user info

--- app/src/main/java/com/example/menaadel/firebaselogin/MainActivity.java
+++ app/src/main/java/com/example/menaadel/firebaselogin/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.menaadel.firebaselogin;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -25,6 +32,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -34,19 +42,24 @@ public class MainActivity extends AppCompatActivity {
     private CallbackManager mCallbackManager;
     private static final String TAG = "AppCompatActivity";
     private FirebaseAuth mAuth;
-
-
+    LoginButton loginButton;
+    Button email;
+    ImageView profileImage;
+    TextView nameAndSurname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email = findViewById(R.id.button2);
+        profileImage = findViewById(R.id.profileImage);
+        nameAndSurname = findViewById(R.id.nameAndSurname);
 
         mAuth = FirebaseAuth.getInstance();
 
         // Initialize Facebook Login button
         mCallbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = findViewById(R.id.button_facebook_login);
+        loginButton = findViewById(R.id.button_facebook_login);
         loginButton.setReadPermissions("email", "public_profile");
         loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -58,13 +71,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancel() {
                 Log.d(TAG, "facebook:onCancel");
-                // ...
             }
 
             @Override
             public void onError(FacebookException error) {
                 Log.d(TAG, "facebook:onError", error);
-                // ...
             }
         });
     }
@@ -88,17 +99,28 @@ public class MainActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
-            updateUI();
+            updateUI(currentUser);
         }
 
     }
 
-    public void updateUI(){
+    public void updateUI(FirebaseUser user){
         Toast.makeText(getApplicationContext(),"you are logged in",Toast.LENGTH_LONG).show();
+        loginButton.setVisibility(View.INVISIBLE);
+        email.setVisibility(View.INVISIBLE);
+        nameAndSurname.setVisibility(View.VISIBLE);
+        profileImage.setVisibility(View.VISIBLE);
 
-        Intent intent = new Intent(MainActivity.this,ProfileLoggedByFB.class);
+        Uri imagePath = user.getPhotoUrl();
+        //Log.d("Path",imagePath);
+        String userName = user.getDisplayName();
+
+        nameAndSurname.setText(userName);
+        Picasso.with(this).load(imagePath).placeholder(R.drawable.movie).into(profileImage);
+
+        /*Intent intent = new Intent(MainActivity.this,ProfileLoggedByFB.class);
         startActivity(intent);
-        finish();
+        finish();*/
     }
 
     private void handleFacebookAccessToken(AccessToken token) {
@@ -113,12 +135,14 @@ public class MainActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+
+                            updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-                            updateUI();
+
                         }
 
                         // ...
--- app/src/main/java/com/example/menaadel/firebaselogin/Profile.java
+++ app/src/main/java/com/example/menaadel/firebaselogin/Profile.java
@@ -2,12 +2,20 @@ package com.example.menaadel.firebaselogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
+
+    TextView profileName;
+    ImageView profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        profileName = findViewById(R.id.nameAndSurname);
+        profileImage = findViewById(R.id.profileImage);
     }
 }
